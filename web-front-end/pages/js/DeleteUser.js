aa22dd317404a79fb3e371e2b41f6020fc3aa871
Raunak567 <www.seema104@gmail.com>
2024-09-03 21:18:54 +0530

    yo

--- web-front-end/pages/js/DeleteUser.js
+++ web-front-end/pages/js/DeleteUser.js
@@ -1,6 +1,7 @@
 document.getElementById("delete-user-form").addEventListener("submit", async (event) => {
 	event.preventDefault();
 	const username = document.getElementById("other-username").value;
+	console.log(username)
 	const response = await fetch(`http://localhost:4001/auth/delete/user`, {
 		method: "DELETE",
 		headers: {
@@ -8,7 +9,8 @@ document.getElementById("delete-user-form").addEventListener("submit", async (ev
 		},
 		body: JSON.stringify({
 			username
-		})
+		}),
+		credentials: "include"
 	});
 		
 });
